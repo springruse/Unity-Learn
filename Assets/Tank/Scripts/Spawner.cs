39b2fd0c957b9b5a5c81767e4dbfdd75b6942e5f
springruse <152939330+springruse@users.noreply.github.com>
2026-01-07 14:22:29 -0700

    added folders and files
    tank folder
    world folder

---
+++ Assets/Tank/Scripts/Spawner.cs
@@ -0,0 +1,32 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] float time = 1.0f;
+    [SerializeField] GameObject spawnObject;
+
+    private void Awake()
+    {
+        print("awake");
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            Vector3 position = transform.position;
+            position.x += Random.Range(-5.0f, 5.0f);
+            position.z += Random.Range(-5.0f, 5.0f);
+            Instantiate(spawnObject, position, transform.rotation);
+        }
+        
+    }
+}
